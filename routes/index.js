e628804b9ef3a4043b5633a56fcde5e044dc8129
root <root@localhost.localdomain>
2019-11-09 16:24:52 +0800

    Second update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Heroku' });
+router.get('/', async function (req, res, next) {
+
+  let showTiTle = 'Heroku~';
+   fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+    fs.readFile('mynewfile1.txt', function (err, data) {
+     showTiTle = data;
+     console.log('read!',showTiTle);
+     res.render('index', { title: showTiTle });
+   });
+  });
+
+
+
 });
 
 module.exports = router;
